b6315621fd6651e7daa2df1ebe66c60b42fe6576
somerongit <someron.bakuli05@gmail.com>
2022-07-10 21:06:43 +0530

    Deleting by name

--- model/create.go
+++ model/create.go
@@ -4,6 +4,16 @@ func CreateTodo(name, todo string) error {
 	insertQ, err := con.Query(`INSERT INTO TODO VALUES(?,?)`, name, todo)
 	defer insertQ.Close()
 	if err != nil {
+		print("model/create CreateTodo :: ", err)
+		return err
+	}
+	return nil
+}
+func DeleteTodo(name string) error {
+	insertQ, err := con.Query(`DELETE FROM TODO WHERE name=?`, name)
+	defer insertQ.Close()
+	if err != nil {
+		print("model/create DeleteTodo :: ", err)
 		return err
 	}
 	return nil
